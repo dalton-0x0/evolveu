654810afaf2acda8f4947d91dd4f32b02a84d4d6
Dalton Okechukwu <Dalton@Dalton-Mac-Air.local>
2019-10-03 09:18:22 -0600

    added event listeners

--- javascript/basic.js
+++ javascript/basic.js
@@ -1,10 +1,58 @@
-let input = document.getElementById("input");
-let button = document.getElementById("clickButton");
+//let numberInput = Number(userInput.value);
+console.log("Hello World from basic.js");
 
-console.log("Hello World form basic.js");
+let button = document.getElementById("clickButton");
+let mouseOver = document.getElementById("main");
+let userInput = document.getElementById("input");
+
+button.addEventListener("click", onButtonClicked);
+mouseOver.addEventListener("mouseover", mouseChangeText);
+mouseOver.addEventListener("mouseout", mouseReturnText);
+userInput.addEventListener("keypress", onPressEnter);
+
+function mouseChangeText() {
+  document.getElementById("main").innerHTML = "Moused Over!";
+}
+
+function mouseReturnText() {
+  document.getElementById("main").innerHTML = "Returned to Original Text!";
+}
 
 function onButtonClicked() {
-  console.log("I am the button clicked event");
-  console.log(input.value);
+  if (inputLength() > 0) {
+    console.log("I am the button clicked event");
+    collectUserInput();
+  }
 }
-button.addEventListener("click", onButtonClicked);
+
+function onPressEnter(event) {
+  if (event.keyCode === 13) {
+    console.log("I am the keypress event");
+    collectUserInput();
+    // onButtonClicked();
+  }
+}
+
+function collectUserInput() {
+  let numInput = Number(userInput.value);
+  console.log(numInput + 1);
+  console.log(size(numInput + 1));
+  let result = size(numInput + 1);
+  document.getElementById("msg").innerHTML = result;
+  userInput.value = "";
+}
+
+function size(n) {
+  if (n < 10) {
+    return n + " is small";
+  } else if (n < 20) {
+    return n + " is med";
+  } else {
+    return n + " is large";
+  }
+  // n < 10 ? "small" : n > 10 ? "med" : "large";
+}
+
+function inputLength() {
+  return userInput.value.length;
+}
